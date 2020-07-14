12e531d1625f59f8bc5a30d97577fba28bff4429
mikhailP <hipopopotatam@gmail.com>
2020-07-15 00:04:53 +0300

    fancy design

--- app/src/main/java/com/example/iikoapi/general/MenuFragment.kt
+++ app/src/main/java/com/example/iikoapi/general/MenuFragment.kt
@@ -1,22 +1,25 @@
 package com.example.iikoapi.general
 
 import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.iikoapi.R
 import com.example.iikoapi.general.menuadapter.MenuAdapter
 import com.example.iikoapi.general.merch.MerchAdapter
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.android.synthetic.main.fragment_menu.*
+
 
 class MenuFragment(var position : Int, var contextMy: Context) : Fragment() {
 
+    private var height : Int = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_menu, container, false)
@@ -24,12 +27,41 @@ class MenuFragment(var position : Int, var contextMy: Context) : Fragment() {
         val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
         val merch = view.findViewById<ViewPager2>(R.id.merchViewPager)
 
-//        viewPager2.reduceDragSensitivity()
-//        viewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
-//        viewPager2.adapter = MenuAdapter(dd.values.toList())
 
+//        val merch = ViewPager2(contextMy)
+//        val merchParams = CollapsingToolbarLayout.LayoutParams(CollapsingToolbarLayout.LayoutParams.MATCH_PARENT, (height * 0.15).toInt())
+//        merchParams.collapseMode = CollapsingToolbarLayout.LayoutParams.COLLAPSE_MODE_PARALLAX
+//        merchParams.parallaxMultiplier = 0.75f
+//        merch.layoutParams = merchParams
+//        collapsing.addView(merch)
         merch.adapter = MerchAdapter(mappedMenu.values.elementAt(0), contextMy)
 
+
+        Log.d("tag", "fssdf - " + (height * 0.15).toInt())
+//        val toolbar = Toolbar(contextMy)
+//        val toolbarParams = CollapsingToolbarLayout.LayoutParams(CollapsingToolbarLayout.LayoutParams.MATCH_PARENT, 0)
+//        toolbarParams.collapseMode = CollapsingToolbarLayout.LayoutParams.COLLAPSE_MODE_PIN
+//        toolbarParams.gravity = Gravity.TOP
+//        toolbarParams.setMargins(0, 0, 0, 55 + 24)
+//        toolbar.popupTheme = android.R.style.ThemeOverlay_Material_Light
+//        toolbar.layoutParams = toolbarParams
+//        collapsing.addView(toolbar)
+//
+//
+//
+//        val tabLayout = TabLayout(contextMy)
+//        val tabLayoutParams = CollapsingToolbarLayout.LayoutParams(CollapsingToolbarLayout.LayoutParams.MATCH_PARENT, CollapsingToolbarLayout.LayoutParams.WRAP_CONTENT)
+//        tabLayout.setPadding(10, 0, 10, 0)
+//        tabLayout.clipToPadding = false
+//        tabLayout.setSelectedTabIndicator(R.drawable.selected_tab)
+//        tabLayout.isTabIndicatorFullWidth = true
+//        toolbarParams.gravity = Gravity.BOTTOM
+////        tabLayout.tabIndicatorGravity = TabLayout.INDICATOR_GRAVITY_STRETCH
+//        tabLayout.layoutParams = tabLayoutParams
+//        collapsing.addView(tabLayout)
+
+
+        viewPager2.adapter = MenuAdapter(mappedMenu.values.toList())
         viewPager2.adapter = MenuAdapter(mappedMenu.values.toList())
         viewPager2.setCurrentItem(position, false)
 
@@ -42,6 +74,16 @@ class MenuFragment(var position : Int, var contextMy: Context) : Fragment() {
         }.attach()
 
         return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val display: Display = activity!!.getWindowManager().getDefaultDisplay()
+        val size = Point()
+        display.getSize(size)
+        val width = size.x
+        height= size.y
+
     }
 
     fun ViewPager2.reduceDragSensitivity() {
