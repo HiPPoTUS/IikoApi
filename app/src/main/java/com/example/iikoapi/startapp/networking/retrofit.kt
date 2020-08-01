735dcded42866b96ac3fb7c2fe9601021335e7de
mikhailP <hipopopotatam@gmail.com>
2020-08-01 20:35:53 +0300

    pop up order dialog

--- app/src/main/java/com/example/iikoapi/startapp/networking/retrofit.kt
+++ app/src/main/java/com/example/iikoapi/startapp/networking/retrofit.kt
@@ -1,5 +1,8 @@
 package com.example.iikoapi.startapp.networking
 
+import android.location.Address
+import com.example.iikoapi.startapp.datatype.OrderInfo
+import com.example.iikoapi.startapp.datatype.OrderRequest
 import com.example.iikoapi.startapp.datatype.OrganisationInfo
 import retrofit2.Call
 import retrofit2.http.*
@@ -27,5 +30,31 @@ interface IIKO_API {
         @Path("org") orgID: String?,
         @Query("access_token") token: String?
     ): Call<MenuResponse?>?
+
+    @POST("add?/")
+    fun post_order(
+        @Query("access_token") token: String?,
+        @Body order:OrderRequest
+    ):Call<OrderInfo>
+
+//    @POST("/orders/checkCreate?")
+//    fun check_order (
+//        @Query("access_token") token: String?,
+//        @Body order:OrderRequest
+//    ):Call<OrderChecksCreationResult>
+//
+//    @POST("orders/checkAddress?")
+//    fun check_delivery(
+//        @Query("access_token") token: String?,
+//        @Query("organizationId") org: String?,
+//        @Body addr: com.example.dodocopy.dataTypes.Address
+//    ):Call<AddressCheckResult>
+//
+//    @GET("orders/deliveryHistoryByPhone?")
+//    fun history(
+//        @Query("access_token") token: String?,
+//        @Query("organization") org: String?,
+//        @Query("phone") phone: String?
+//    ):Call<CustomersDeliveryHistoryResponse>
 }
 
